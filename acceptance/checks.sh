5615e895a89d33c89e91b0eaf7864086754046e9
Callum James Tait <callum.tait@photobox.com>
2021-06-23 22:47:04 +0100

    refactor: adjusting timeouts on checks

--- acceptance/checks.sh
+++ acceptance/checks.sh
@@ -9,11 +9,11 @@ echo "Checking if RunnerDeployment repo test is set"
 if [ "${TEST_REPO}" ] && [ ! "${USE_RUNNERSET}" ]; then
   runner_name=
   count=0
-  while [ $count -le 30 ]; do
+  while [ $count -le 10 ]; do
     echo "Finding Runner ..."
     runner_name=$(kubectl get runner --output=jsonpath="{.items[*].metadata.name}")
     if [ "${runner_name}" ]; then
-      while [ $count -le 30 ]; do
+      while [ $count -le 10 ]; do
         runner_pod_name=
         echo "Found Runner \""${runner_name}"\""
         echo "Finding underlying pod ..."
@@ -24,55 +24,57 @@ if [ "${TEST_REPO}" ] && [ ! "${USE_RUNNERSET}" ]; then
           kubectl wait pod/${runner_pod_name} --for condition=ready --timeout 270s
           break 2
         fi
-        sleep 1
+        sleep 5
         let "count=count+1"
       done
     fi
-    sleep 1
+    sleep 5
     let "count=count+1"
   done
-  if [ $count -ge 30 ]; then
+  if [ $count -ge 10 ]; then
+    echo "Timeout reached, failed to find the RunnerDeployment or underlying pod"
     repo_runnerdeployment_passed=false
   else
     repo_runnerdeployment_passed=true
   fi
+fi
+
 echo "Checking if RunnerSet repo test is set"
-elif [ "${TEST_REPO}" ] && [ "${USE_RUNNERSET}" ]; then
+if [ "${TEST_REPO}" ] && [ "${USE_RUNNERSET}" ]; then
   runnerset_name=
   count=0
-  while [ $count -le 30 ]; do
+  while [ $count -le 10 ]; do
     echo "Finding RunnerSet ..."
     runnerset_name=$(kubectl get runnerset --output=jsonpath="{.items[*].metadata.name}")
     if [ "${runnerset_name}" ]; then
-      while [ $count -le 30 ]; do
+      while [ $count -le 10 ]; do
         runnerset_pod_name=
         echo "Found RunnerSet \""${runnerset_name}"\""
         echo "Finding underlying pod ..."
         runnerset_pod_name=$(kubectl get pod --output=jsonpath="{.items[*].metadata.name}" | grep ${runnerset_name})
-        echo "BEFORE IF"
         if [ "${runnerset_pod_name}" ]; then
-          echo "AFTER IF"
           echo "Found underlying pod \""${runnerset_pod_name}"\""
           echo "Waiting for pod \""${runnerset_pod_name}"\" to become ready..."
           kubectl wait pod/${runnerset_pod_name} --for condition=ready --timeout 270s
           break 2
         fi
-      sleep 1
+      sleep 5
       let "count=count+1"
       done
     fi
-    sleep 1
+    sleep 5
     let "count=count+1"
   done
-  if [ $count -ge 30 ]; then
+  if [ $count -ge 10 ]; then
+    echo "Timeout reached, failed to find the RunnerSet or underlying pod"
     repo_runnerset_passed=false
   else
     repo_runnerset_passed=true
   fi
 fi
 
-if [ ${repo_runnerset_passed} == true ] || [ ${repo_runnerset_passed} == "skipped" ] && \
-   [ ${repo_runnerdeployment_passed} == true ] || [ ${repo_runnerdeployment_passed} == "skipped" ]; then
+if [ "${repo_runnerset_passed}" == "true" ] || [ "${repo_runnerset_passed}" == "skipped" ] && \
+   [ "${repo_runnerdeployment_passed}" == "true" ] || [ "${repo_runnerdeployment_passed}" == "skipped" ]; then
   echo "INFO : All tests passed or skipped"
   echo "RunnerSet Repo Test Status : ${repo_runnerset_passed}"
   echo "RunnerDeployment Repo Test Status : ${repo_runnerdeployment_passed}"
